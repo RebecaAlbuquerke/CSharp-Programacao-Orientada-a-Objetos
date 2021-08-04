29a99574db9d0360086803facd11da65b4c11f11
Rebeca Albuquerque <rebecaalbuquerke@gmail.com>
2021-08-04 13:47:26 -0300

    Exercicio 02

---
+++ Exercicio-02/Exercicio-02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var funcionario1 = new Funcionario();
+            var funcionario2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionario:");
+            Console.Write("Nome: ");
+            funcionario1.Nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            funcionario1.Salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionario:");
+            Console.Write("Nome: ");
+            funcionario2.Nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            funcionario2.Salario = double.Parse(Console.ReadLine());
+
+            double salariomedio = (funcionario1.Salario + funcionario2.Salario) / 2.0;
+
+            Console.WriteLine("O salario medio é "+ salariomedio);
+        }
+    }
+}
