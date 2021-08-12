e0f4fdfbc26cb3de35e0df05687fcf1a455900e0
Rebeca Albuquerque <rebecaalbuquerke@gmail.com>
2021-08-12 13:39:14 -0300

    Exercicio 04

---
+++ Exercicio-04/Exercicio-04/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_04
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public double SalarioBruto { get; set; }
+        public double Imposto { get; set; }
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", R$ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio-04/Exercicio-04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var funcionario = new Funcionario();
+
+            Console.Write("Digite o nome: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Digite o salario bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite o imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Os dados do funcionário: " + funcionario);
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            funcionario.AumentarSalario(porcentagem);
+       
+            Console.WriteLine("Dados atualizados do funcionário: " + funcionario);
+        }
+    }
+}
