84fc00239700f754dc20328680b90e3a96c850c5
Rebeca Albuquerque <rebecaalbuquerke@gmail.com>
2021-08-12 13:23:33 -0300

    Exercicio 03

---
+++ Exercicio-03/Exercicio-03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var retangulo = new Retangulo();
+
+            Console.WriteLine("Digite a largura e altura do retângulo: ");
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("A area é igual a " + retangulo.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("O perimetro é igual a " + retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("A diagonal é igual a " + retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
